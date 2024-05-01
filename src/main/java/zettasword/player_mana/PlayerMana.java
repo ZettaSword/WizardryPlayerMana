debab745d46ccb7a803704cc5deaa538f3c7b9af
Zetta <zettasword@mail.ru>
2024-05-01 18:39:28 +0300

    Changed version to 1.1.0

--- src/main/java/zettasword/player_mana/PlayerMana.java
+++ src/main/java/zettasword/player_mana/PlayerMana.java
@@ -19,7 +19,7 @@ import zettasword.player_mana.proxy.CommonProxy;
 public class PlayerMana {
     public static final String MODID = "player_mana";
     public static final String NAME = "Player Mana";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @SidedProxy(clientSide = "zettasword.player_mana.proxy.ClientProxy",
             serverSide = "zettasword.player_mana.proxy.CommonProxy")
